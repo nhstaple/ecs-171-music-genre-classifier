c9ebb5532022775788056adaa689660c33cc77aa
Nick S <nicholas.stapleton97@gmail.com>
2019-11-17 11:08:48 -0800

    included dm interface update

--- DataManagement/CSVInterface.py
+++ DataManagement/CSVInterface.py
@@ -9,21 +9,20 @@ class featRead:
 		self.tableDF = {}
 		for key in csvs:
 			if(key == 'features.csv'):
-				print('Loading features.csv')
+				print('Reading features.csv')
 				features = pd.read_csv('features.csv', index_col=0, header=[0,1,2])
 				self.tableDF['features'] = features
 
 			elif(key == 'tracks.csv'):
-				print('Loading tracks.csv')
+				print('Reading tracks.csv')
 				tracks = pd.read_csv('tracks.csv', index_col=0, header=[0, 1])
 				self.tableDF['album'] = tracks['album'].copy()
 				self.tableDF['track'] = tracks['track'].copy()
 				self.tableDF['artist'] = tracks['artist'].copy()
 				self.tableDF['set'] = tracks['set'].copy()
-				# self.tableDF['genre_top'] = tracks['genre_top'].copy()
 
 			elif(key == 'echonest.csv'):
-				print('Loading echonest.csv')
+				print('Reading echonest.csv')
 				echonest = pd.read_csv('echonest.csv', index_col=0, header=[0, 1, 2])
 				echo_audio_feat = echonest.iloc[:, echonest.columns.get_level_values(1) == 'audio_features'].copy()
 				echo_meta_data = echonest.iloc[:, echonest.columns.get_level_values(1) == 'metadata'].copy()
@@ -34,12 +33,12 @@ class featRead:
 
 
 			elif(key == 'genres.csv'):
-				print('Loading genres.csv')
+				print('Reading genres.csv')
 				genres = pd.read_csv('genres.csv', index_col=0)
 				self.tableDF['genres'] = genres
 			else:
 				print(key + ' is not a vaild csv file name')
-				break
+				break;
 
 	#lists all of the data frames names contained by the featRead object
 	def listFrames(self):
@@ -83,6 +82,14 @@ class featRead:
 			subset = subset.loc[(subset['subset'] == sub) | (subset['subset'] == 'medium') | (subset['subset'] == 'small')]
 			newDF = frame[frame.index.isin(subset.index)].copy()
 			return newDF
+		elif(sub == 'cleanLarge'):
+			genres = self.tableDF['track']
+			genres = genres['genre_top']
+			#full set genre_top not complete
+			genres = genres.dropna()
+
+			newDF = frame[frame.index.isin(genres.index)].copy()
+			return newDF
 
 		else:
 			print('Not a vaild set type')
@@ -109,6 +116,4 @@ class featRead:
 	def mergeFrames(self, f1, f2):
 		combined = pd.merge(f1, f2, on='track_id', how='outer')
 		return combined
-
-
 
