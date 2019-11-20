07ab4ea5fc63c3570aa5af32ca117f849b46005f
Matt Marlow <mfmarlow@ucdavis.edu>
2019-11-19 16:55:41 -0800

    pickle path update

--- Data_Management/makePick.py
+++ Data_Management/makePick.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 print('trying to read tracks.csv')
 try:
-	tracks = pd.read_csv('tracks.csv', index_col=0, header=[0, 1])
+	tracks = pd.read_csv('../Data/tracks.csv', index_col=0, header=[0, 1])
 	print('saving as tracks.pkl')
-	tracks.to_pickle('tracks.pkl')
+	tracks.to_pickle('../Data/tracks.pkl')
 except:
 	print('tracks conversion failed')
 
@@ -13,9 +13,9 @@ except:
 
 print('trying to read features.csv')
 try:	
-	features = pd.read_csv('features.csv', index_col=0, header=[0,1,2])
+	features = pd.read_csv('../Data/features.csv', index_col=0, header=[0,1,2])
 	print('saving as features.pkl')
-	features.to_pickle('features.pkl')
+	features.to_pickle('../Data/features.pkl')
 except:
 	print('features conversion failed')
 
@@ -23,17 +23,17 @@ except:
 
 print('trying to read echonest.csv')
 try:
-	echonest = pd.read_csv('echonest.csv', index_col=0, header=[0,1,2])
+	echonest = pd.read_csv('../Data/echonest.csv', index_col=0, header=[0,1,2])
 	print('saving as echonest.pkl')
-	echonest.to_pickle('echonest.pkl')
+	echonest.to_pickle('../Data/echonest.pkl')
 except:
 	print('echonest conversion failed!')
 
 
 print('trying to read genres.csv')
 try:
-	genres = pd.read_csv('genres.csv', index_col=0)
+	genres = pd.read_csv('../Data/genres.csv', index_col=0)
 	print('saving as genres.pkl')
-	genres.to_pickle('genres.pkl')
+	genres.to_pickle('../Data/genres.pkl')
 except:
 	print('genres conversion failed!')
